5f0d14e20cdcecddf5f8000949ac715c11a282ed
AndreHilario <andre18hilario@gmail.com>
2023-01-12 10:23:36 -0300

    Versão com pedido do número correto de cartas

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const jogoMemoria = document.querySelector('.jogo-da-memoria');
 
-const listaCards = [];
+const listaCards = ['back', 'bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 
+'tripletsparrot', 'unicornparrot', 'back', 'bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 
+'tripletsparrot', 'unicornparrot'];
+
+let numCard = 0;
+
+function pedirCartas(){
+    const qtdCartas = prompt("Digite o número de cartas que quer jogar(de 4 a 14)");
+    numCard = Number(qtdCartas);
+    if(numCard === 4 || numCard === 6 || numCard === 8 || numCard === 10 || numCard === 12 || numCard === 14){
+        console.log('1');
+    } else {
+        alert("Você digitou um número inválido");
+        pedirCartas();
+    }
+}
+pedirCartas();
 
