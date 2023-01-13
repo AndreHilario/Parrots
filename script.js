674d9f4956103224a98d8ff867b664a2399b7b94
AndreHilario <andre18hilario@gmail.com>
2023-01-13 14:34:01 -0300

    Versão com as cartas girando ao clicar

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const listaCards = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparr
 'tripletsparrot', 'unicornparrot'];
 
 
+function cartasAleatorias(){
+
+    return Math.random() -0.5;
+}
+
 let numCard = 0;
 
 const cartas = document.querySelector('.jogo-da-memoria');
@@ -12,10 +17,11 @@ console.log(cartas);
 function criarCartas(){
 
     let template = `
-        <div class="carta">
-            <div class="face-card front-face-card"> </div>
+        <div onclick="virarCarta(this)" class="carta">
+            <div class="face-card front-face-card"><img src="img/${listaCards[1]}.gif"></div>
             <div class="face-card back-face-card"><img src="img/back.png"></div>
-        </div>`;
+        </div>
+        `;
 
     cartas.innerHTML = cartas.innerHTML + template;
       
@@ -37,9 +43,25 @@ function pedirCartas(){
 }
 pedirCartas();
 
-function cartasAleatorias(){
 
-    return Math.random() -0.5;
+function virarCarta(cartaClicada){
+
+
+    const cartaAnterior = document.querySelector('.jogo-da-memoria .clicado');
+    
+    if (cartaAnterior !== null){
+
+        cartaAnterior.classList.remove('clicado');
+        
+    } 
+    cartaClicada.classList.add('clicado');
+
+    const cartaGiro = document.querySelector('.jogo-da-memoria .clicado .back-face-card');
+    if(cartaGiro.parentNode.classList.contains('clicado')){
+        cartaGiro.classList.add('girar-back-face-card');
+    } 
+    
+    
 }
 
 
